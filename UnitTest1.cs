10f1dd53f81324cc81714c1873bcaf141683dd7c
Sowmya Gargatti <sowmya.gargatti@biocair.com>
2021-09-16 21:26:35 +0100

    1

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,10 +1,17 @@
 using System;
 using Xunit;
-
+using FirstAPICode.Controllers;
 namespace FirstAPICodeTest.Test
 {
     public class UnitTest1
     {
+        WeatherForecastController controller = new WeatherForecastController();
+        [Fact]
+        public void GetTodayWeather()
+        {
+            var returnValue =controller.Get(1);
+            Asset.Equal("cool",returnValue.value);
+        }
         [Fact]
         public void Test1()
         {
